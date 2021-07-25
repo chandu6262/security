385687556f3fe6e50289b4bd623ce44c58c77637
Chandu <Chandu@LAPTOP-K69SN8IF>
2021-07-25 11:10:08 +0530

    first commit

---
+++ securitydbexample/src/main/java/com/techprimers/security/securitydbexample/SecuritydbexampleApplication.java
@@ -0,0 +1,13 @@
+package com.techprimers.security.securitydbexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuritydbexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuritydbexampleApplication.class, args);
+	}
+
+}
---
+++ securitydbexample/src/test/java/com/techprimers/security/securitydbexample/SecuritydbexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techprimers.security.securitydbexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuritydbexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
